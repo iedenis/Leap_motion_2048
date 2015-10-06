5b8733e236aa724814950f0dd336d7725099ca3a
Denis <denis.ievlev@gmail.com>
2015-10-06 18:59:10 +0300

    Cleaning the code. Deleting some unnecessary files

--- js/leapController.js
+++ js/leapController.js
@@ -1,4 +1,3 @@
-
 function leapInputManager(){
    this.events={};
     this.listen();
@@ -13,111 +12,70 @@ leapInputManager.prototype.emit=function(event,data)
     });
   }
 };
-var controller = Leap.loop({enableGestures: true}, function(frame){
-    controller.setBackground(true); 
-  if(frame.valid && frame.gestures.length > 0){
-    frame.gestures.forEach(function(gesture){
-        switch (gesture.type){
-          case "circle":
-            //  console.log("Circle Gesture");
-              break;
-          case "keyTap":
-            //  console.log("Key Tap Gesture");
-              break;
-          case "screenTap":
-           //   console.log("Screen Tap Gesture");
-              break;
-          case "swipe":
-        //      console.log("Swipe Gesture");
-              break;
-        }
-    });
-  }
+
+var controller = new Leap.Controller({
+  host: '127.0.0.1',
+  port: 6437,
+  enableGestures: true,
+  frameEventName: 'animationFrame',
+  useAllPlugins: true
 });
-/*
-leapInputManager.prototype.listen=function(){
-    
-    controller.on("gesture",function(gesture){
-        if(gesture.type=="swipe"){
-            console.log("This is swipe gesture");
-            var isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
-          //Classify as right-left or up-down
-          if(isHorizontal){
-              if(gesture.direction[0] > 0){
-                  swipeDirection = "right";
-                  console.log("right");
-              } else {
-                  swipeDirection = "left";
-                this.emit("move");
-                  console.log(this.emit("move",3));
-              }
-          } else { //vertical
-              if(gesture.direction[1] > 0){
-                  swipeDirection = "up";
-                  
-                  console.log("up");
-
-              } else {
-                swipeDirection = "down";
-               //   GameManager.prototype.move(2);
-                  this.emit("move",2);
-                  console.log("down");
-              }                  
-          }
-     //   console.log(swipeDirection);
-      }
-    });
-};
-
-/*leapInputManager.prototype.on=function(event,callback){
-
-if (!this.events[event]) {
-    this.events[event] = [];
-  }
-  this.events[event].push(callback);
-};
-controller.connect();*/
 leapInputManager.prototype.listen=function(){
         var self=this;
-
+var d = new Date();
+var now = d.getTime();
+var prevSwipeTime = now;
+var prevGesture="";
 controller.on("gesture",function(gesture){
+    
         if(gesture.type=="swipe"){
-            console.log("This is swipe gesture");
             var isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
           //Classify as right-left or up-down
           if(isHorizontal){
               if(gesture.direction[0] > 0){
                   swipeDirection = "right";
+                  if(gesture.id==1){
                   console.log("right");
                   self.emit("move",1);
-              } else {
+                  }
+              } else {  
+                  if(gesture.id==1){
                   swipeDirection = "left";
                 self.emit("move",3);
-                  console.log(self.emit("move",3));
+                  console.log("left");
+                  }
               }
+              
           } else { //vertical
               if(gesture.direction[1] > 0){
                   swipeDirection = "up";
+                  var array=new Array;
+                  if(gesture.id==1){
                   self.emit("move",0);
                   console.log("up");
-
+                  console.log(gesture.id);
+                  }
               } else {
+                  if(gesture.id==1){
                 swipeDirection = "down";
-               //   GameManager.prototype.move(2);
-                  this.emit("move",2);
+                  self.emit("move",2);
                   console.log("down");
+                      console.log(gesture.id);
+                  }
               }                  
           }
-     //   console.log(swipeDirection);
         }
-    if(gesture.type=="screenTap"||gesture.type=="keyTap"){
-        console.log("this is keyTap");
+    if(gesture.type=="keyTap"){
+        console.log("****restarting the game******");
         self.emit("restart");
     }
     });
     //restarting the game on pressing button restart-button
  this.bindButtonPress(".restart-button", this.restart);
-
+this.bindButtonPress(".keep-playing-button",this.keepPlaying);
+    
+//restarting the game after finish//    
+this.bindButtonPress(".retry-button", this.restart);
 }
 //end listen function
 
@@ -143,6 +101,10 @@ controller.connect();
 leapInputManager.prototype.restart = function (event) {
   this.emit("restart");
 };
+
+leapInputManager.prototype.keepPlaying=function(event){
+    this.emit("restart");
+}
 
 leapInputManager.prototype.bindButtonPress = function (selector, fn) {
   var button = document.querySelector(selector);
